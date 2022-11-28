a9409f99b9bbad4e3a23f053cfd0a616abfd1fab
Albert <zan15051@gmail.com>
2022-11-28 20:37:42 +0700

    Changed project folder structure, renamed namespace to a proper one, returned .sln file

---
+++ RuTrainer/RuTrainer/ViewModels/SearchViewModel.cs
@@ -0,0 +1,25 @@
+﻿using RuTrainer.Models;
+using RuTrainer.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
+
+namespace RuTrainer.ViewModels
+{
+    public class SearchViewModel : BaseViewModel
+    {
+        public ICommand SearchCommand { get; }
+        public ObservableCollection<Station> NearbyStations { get; }
+        public SearchViewModel()
+        {
+            var ns = ApiProvider.GetNearbyStationsAsync();
+
+            NearbyStations = new ObservableCollection<Station>((IEnumerable<Station>)ns);
+            //SearchCommand = new Command(async () => await ApiProvider.);
+        }
+    }
+}
