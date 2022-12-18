9d766e459e3fee723a177a78bc0d5bc456eafcd5
Albert <zan15051@gmail.com>
2022-12-19 04:48:57 +0700

    added from-to bars for searching stations, swap&clear buttons

--- RuTrainer/RuTrainer/ViewModels/SearchViewModel.cs
+++ RuTrainer/RuTrainer/ViewModels/SearchViewModel.cs
@@ -15,55 +15,136 @@ namespace RuTrainer.ViewModels
 {
     public class SearchViewModel : BaseViewModel
     {
-        private string _searchText = String.Empty;
-        private ObservableCollection<Station> _stationsList;
-        private Command searchCommand;
-        public string SearchText
+        private string searchBar;
+        private string _searchFromText = String.Empty;
+        private string _searchToText = String.Empty;
+        private ObservableCollection<Station> _stationList;
+        private Station? _stationsListSelectedItem;
+        private Command _searchCommand;
+        private Station _stationFrom;
+        private Station _stationTo;
+        public Station? StationsListSelectedItem
         {
-            get => _searchText; 
+            get => _stationsListSelectedItem;
             set
             {
-                SetProperty(ref _searchText, value);
-                Search(_searchText);
+                SetProperty(ref _stationsListSelectedItem, value);
+                SetSearchBarText();
+                StationList.Clear();
             }
         }
-        public ObservableCollection<Station> StationsList
+        private void SetSearchBarText()
         {
-            get => _stationsList;
-            set => SetProperty(ref _stationsList, value);
+            if (searchBar == "from")
+            {
+                _stationFrom = StationsListSelectedItem;
+                SearchFromText = _stationFrom.title;
+                return;
+            }
+            _stationTo = StationsListSelectedItem;
+            SearchToText = _stationTo.title;
+        }
+        public string SearchFromText
+        {
+            get => _searchFromText; 
+            set
+            {
+                SetProperty(ref _searchFromText, value);
+                Search("from");
+            }
+        }
+        public string SearchToText
+        {
+            get => _searchToText;
+            set
+            {
+                SetProperty(ref _searchToText, value);
+                Search("to");
+            }
+        }
+        public ObservableCollection<Station> StationList
+        {
+            get => _stationList;
+            set => SetProperty(ref _stationList, value);
         }        
-        public ICommand SearchCommand => searchCommand ??= new Command(Search);
+        public ICommand SearchCommand => _searchCommand ??= new Command(Search);
 
         public SearchViewModel()
         {
-            StationsList = new ObservableCollection<Station>();
+            StationList = new ObservableCollection<Station>();
             //LoadAllStationsAsync();
         }
         private void Search(object p)
         {
+            string searchType = p.ToString();
+            searchBar = searchType;
             IsBusy = true;
             Task.Run(() =>
             {
-                StationsList.Clear();
-                if (String.IsNullOrWhiteSpace(SearchText)) return;
-                HandleSearch();
+                StationList.Clear();
+                if (searchType == "from")
+                {
+                    if (String.IsNullOrWhiteSpace(SearchFromText))
+                    {
+                        _stationFrom = null;
+                        return;
+                    }
+                } else 
+                if (String.IsNullOrWhiteSpace(SearchToText))
+                {
+                    _stationTo = null;
+                    return;
+                }
+                HandleSearch(searchType);
             }).ContinueWith((t) =>
             {
                 IsBusy = false;
             });
 
         }
-        private void HandleSearch()
+        private void HandleSearch(string searchType)
         {
+            string SearchText;
+            if (searchType == "from")
+                SearchText = SearchFromText;
+            else SearchText = SearchToText;
+
             var source = ApiProvider.AllStations
                 .Where(x => x.Settlement.title.ToLower().StartsWith(SearchText.ToLower()) ||
                             x.Settlement.Region.title.ToLower().StartsWith(SearchText.ToLower()) ||
                             x.title.ToLower().StartsWith(SearchText.ToLower()));
-            foreach (var item in source)
-                StationsList.Add(item);
+            if (_stationFrom == null ||
+                _stationTo == null)
+                foreach (var item in source)
+                    StationList.Add(item);
+
         }
 
+        private Command clearCommand;
+        public ICommand ClearCommand => clearCommand ??= new Command(Clear);
 
+        private void Clear()
+        {
+            SearchToText = String.Empty;
+            SearchFromText = String.Empty;
+        }
 
+        private Command searchTripsCommand;
+        public ICommand SearchTripsCommand => searchTripsCommand ??= new Command(SearchTrips);
+
+        private void SearchTrips()
+        {
+
+        }
+
+        private Command swapCommand;
+        public ICommand SwapCommand => swapCommand ??= new Command(Swap);
+
+        private void Swap()
+        {
+            var tmp = SearchToText;
+            SearchToText = SearchFromText;
+            SearchFromText = tmp;
+        }
     }
 }
